55b9a7f4efbba6a6fce2beb6102c986a0ebc6834
njdevengine <newjerseydevelopment@gmail.com>
2019-03-21 19:09:51 -0400

    Create sqlalchemy.py

---
+++ sqlalchemy.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy import Column, Integer, String, Float
+
+import pymysql
+pymysql.install_as_MySQLdb()
+
+Base = declarative_base()
+class Dog(Base):
+    __tablename__ = 'dog'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(225))
+    color = Column(String(255))
+    age = Column(Integer)
+    
+myDog = Dog(name="rocco",color="brown",age=3)
+
+engine = create_engine('sqlite:///pets.sqlite')
+conn = engine.connect()
+
+Base.metadata.create_all(engine)
+
+from sqlalchemy.orm import Session
+session = Session(bind=engine)
+
